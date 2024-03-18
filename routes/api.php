9beb62fae199194895c7db9d38d71085529abb9b
unknown <alisaid200144@gmail.com>
2024-03-18 03:17:51 +0200

    hi today

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,8 @@ use App\Http\Controllers\studentController;
 use App\Http\Controllers\studentCourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\AuthMiddleware;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,11 +21,24 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/allCourses', [courseController::class,'index']);
-Route::get('oneCourse/{id}', [courseController::class,'show']);
-Route::post('Add', [courseController::class,'store']);
-Route::put('edit/{id}', [courseController::class,'update']);
-Route::delete('delete/{id}', [courseController::class,'destroy']);
-Route::apiResource('cousre',courseController::class);
-Route::apiResource('student',studentController::class);
-Route::apiResource('studentCourse',studentCourseController::class);
+// Route::get('/allCourses', [courseController::class,'index']);
+// Route::get('oneCourse/{id}', [courseController::class,'show']);
+// Route::post('Add', [courseController::class,'store']);
+// Route::put('edit/{id}', [courseController::class,'update']);
+// Route::delete('delete/{id}', [courseController::class,'destroy']);
+Route::post('register', [AuthController::class,'register']);
+
+Route::post('login', [AuthController::class,'login'])->middleware('api');
+Route::group([
+    'middleware' => ['api',AuthMiddleware::class],
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::apiResource('cousre',courseController::class);
+    Route::apiResource('student',studentController::class);
+    Route::apiResource('studentCourse',studentCourseController::class);
+    Route::get('logout', [AuthController::class,'logout']);
+    Route::get('me', [AuthController::class,'me']);
+
+});
